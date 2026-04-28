ba414bbe00b3b7dc9fe5a254264c49522a8114bb
Ayushman <ayushman.gigs@gmail.com>
2026-04-28 20:02:43 +0530

    changes in apolloClient

--- src/utils/apolloClient.tsx
+++ src/utils/apolloClient.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://downhill-gqll-api.onrender.com' }),
+  link: new HttpLink({ uri: 'https://downhill-gqll-api.onrender.com/query' }),
   cache: new InMemoryCache()
 });
 
